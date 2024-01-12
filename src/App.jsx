7e5dae26d635e5b0c1f3a158af6141b37b201623
mario-c-alvarez <mario.c.alvarez@accenture.com>
2024-01-12 14:58:46 -0600

    se agrega adobe client data layer

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const apponclick= () => {
+    window.digitalData.push({ "event": "App On Click Start" });
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hola sitio en React para probar el Data Layer
+        </p>
+        
+
+        <button onClick={apponclick}>App On Click Start</button>
+
+      </header>
+     
+    </div>
+    
+  );
+}
+
+export default App;
