98af170c311e04b78ba7dc895c67a8bd3b5ecd71
mario-c-alvarez <mario.c.alvarez@accenture.com>
2024-01-12 13:00:05 -0600

    copys...

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola sitio en React para probar el data Layer
         </p>
         <a
           className="App-link"
